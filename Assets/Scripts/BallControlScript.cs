0aee4b31ccb5ed1422b26071cd33d9ac18882751
miceorbirds <45165477+miceorbirds@users.noreply.github.com>
2020-11-16 22:07:26 +0300

    some scripts

--- Assets/Scripts/BallControlScript.cs
+++ Assets/Scripts/BallControlScript.cs
@@ -5,11 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class BallControlScript : MonoBehaviour {
 
-	// Reference to Rigidbody2D component of the ball game object
 	Rigidbody2D rb;
 
-	// Range option so moveSpeedModifier can be modified in Inspector
-	// this variable helps to simulate objects acceleration
 	[Range(0.2f, 2f)]
 	public float moveSpeedModifier = 0.5f;
 
@@ -17,92 +14,58 @@ public class BallControlScript : MonoBehaviour {
 	// as velocity to Rigidbody2d component
 	float dirX, dirY;
 
-	// Reference to Balls Animator component to control animaations transition
 	Animator anim;
-
-	// Setting bool variable that ball is alive at the beginning
 	static bool isDead;
-
-	// Variable to allow or disallow movement when ball is alive or dead
 	static bool moveAllowed;
-
-	// Variable to be set to true if you win
 	static bool youWin;
+	static bool keyPickedUp;
 
-	// Reference to WinText game object to control its appearance
-	// Text game object can be added in inspector because of [SerializeField] line
 	[SerializeField]
 	GameObject winText = default;
+	[SerializeField]
+	GameObject noKeyTip = default;
 
-	// Use this for initialization
 	void Start () {
-
-		// Turn WinText off at the start
 		winText.gameObject.SetActive(false);
-
-		// You don't win at the start
+		noKeyTip.gameObject.SetActive(false);
 		youWin = false;
-
-		// Movement is allowed at the start
 		moveAllowed = true;
-
-		// Ball is alive at the start
 		isDead = false;
-
+		keyPickedUp = false;
 		// Getting Rigidbody2D component of the ball game object
 		rb = GetComponent<Rigidbody2D> ();
-
 		// Getting Animator component of the ball game object
 		anim = GetComponent<Animator> ();
-
 		// Set BallAlive animation
 		anim.SetBool ("BallDead", isDead);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		// Getting devices accelerometer data in X and Y direction
 		// multiplied by move speed modifier
 		dirX = Input.acceleration.x * moveSpeedModifier;
 		dirY = Input.acceleration.y * moveSpeedModifier;
 
-		// if isDead is true
 		if (isDead) {
-
-			// then ball movement is stopped
 			rb.velocity = new Vector2 (0, 0);
-
-			// Set Animators BallDead variable to true to switch to 
 			anim.SetBool ("BallDead", isDead);
-
-			// Restart scene to play again in 1 seconds
 			Invoke ("RestartScene", 1f);
 		}
-
-		// If you win
 		if (youWin) {
-
-			// then turn YouWin sign on
-			winText.gameObject.SetActive (true);
-
-			// ball movement is not allowed anymore
+			winText.gameObject.SetActive(true);
 			moveAllowed = false;
-
-			// switch to Ball Dead Animation so ball falls into exit hole
 			anim.SetBool("BallDead", true);
-
 			// Restart scene to play again in 2 seconds
 			Invoke ("RestartScene", 2f);
 		}
-
 	}
 
 	void FixedUpdate()
 	{
 		// Setting a velocity to Rigidbody2D component according to accelerometer data
 		if (moveAllowed)
-		rb.velocity = new Vector2 (rb.velocity.x + dirX, rb.velocity.y + dirY);
+			rb.velocity = new Vector2 (rb.velocity.x + dirX, rb.velocity.y + dirY);
 	}
 
 	// Method is invoked by DeathHoleScript when ball touches deathHole collider
@@ -112,7 +75,7 @@ public class BallControlScript : MonoBehaviour {
 		isDead = true;
 	}
 
-	// Method is inviked by exit hole game object when ball thouches its collider
+	// Method is invoked by exit hole game object when ball thouches its collider
 	public static void setYouWinToTrue()
 	{
 		youWin = true;
@@ -121,6 +84,24 @@ public class BallControlScript : MonoBehaviour {
 	// Method to restart current scene
 	void RestartScene()
 	{
-		SceneManager.LoadScene ("Puzzle_Scene");
+		SceneManager.LoadScene("Puzzle_Scene");
+	}
+
+	public static void PickUpKey()
+    {
+		keyPickedUp = true;
+
+	}
+
+	public static void OpenLocker()
+	{
+		if (keyPickedUp)
+        {
+			youWin = true;
+		} else
+        {
+			noKeyTip.gameObject.SetActive(true);
+		}
+
 	}
 }
---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+	void OnTriggerEnter2D()
+	{
+		BallControlScript.PickUpKey();
+		Destroy(this.gameObject);
+	}
+}
